42e7c46d9fdac1c81bf339a44aceba473219282b
DavidZomber935 <davidbra12345@gmail.com>
2024-02-19 19:36:58 -0500

    añadir archivo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './screens/home/home.component';
+import { ServiciosComponent } from './screens/servicios/servicios.component';
+import { ContactosComponent } from './screens/contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ServiciosComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
